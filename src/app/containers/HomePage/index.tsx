18a5412401566da5b98d722f9fdb24535f8ee412
saniketprdxn <anikets@prdxn.com>
2022-03-07 13:49:58 +0530

    feat: initial setup

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageContainer } from './Homepage.styled';
+
+type Props = {}
+
+const HomePage = (props: Props) => {
+    return (
+        <PageContainer>HomePage</PageContainer>
+    )
+}
+
+export default HomePage
